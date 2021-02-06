5f6b34fdc0c8bf286ddb819dc286df47ba03b768
Sarah Leong <sleong20@stuy.edu>
2021-02-06 08:08:38 -0500

    made methods static

--- 01-recursion/sqrt.java
+++ 01-recursion/sqrt.java
@@ -1,12 +1,12 @@
 public class sqrt {
-    public double sqrtNewton(double n) { //wrapper
+    public static double sqrtNewton(double n) { //wrapper
         double g = 1;
         while ((g*g)/n > 0.00001) { // checks the percent difference
             g = guessHelp(n, g);
         }
         return g;
     }
-    private double guessHelp(double n, double guess) {
+    private static double guessHelp(double n, double guess) {
         return (n/(guess+guess))/2;
     }
 }
