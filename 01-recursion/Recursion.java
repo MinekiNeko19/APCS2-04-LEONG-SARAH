65260ab96602d7b7c2eea5481e90d08a8e7fad2d
Sarah Leong <sleong20@stuy.edu>
2021-02-06 09:51:40 -0500

    copied code from sqrt.java

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -7,7 +7,18 @@ public class Recursion {
         return 0;
     }
 
-    public static double sqrt(double n) {
-        return 0;   
+    public static double sqrt(double n) { //wrapper
+        if (n<0) throw new ArithmeticException();
+        double g = 1;
+        g = guessHelp(n, g);
+        return g;
+    }
+    private static double guessHelp(double n, double guess) {
+        if (n == 0) return 0;
+        if (Math.abs(guess*guess - n) <= n*0.00001) {
+            return guess;
+        } else {
+            return guessHelp(n, (n/guess+guess)/2);
+        }
     }
 }
