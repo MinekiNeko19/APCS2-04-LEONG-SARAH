970a24b8f08dca6379b48c8414851e7042eb2107
Sarah Leong <sleong20@stuy.edu>
2021-02-10 13:08:27 -0500

    initialized folder

---
+++ 02-queens/QueenBoard.java
@@ -0,0 +1,28 @@
+public class QueenBoard {
+    private int[][] board;
+
+    private boolean addQueen(int r, int c) {
+        return true;
+    }
+    private void removeQueen(int r, int c) {
+
+    }
+
+    // constructor
+    public QueenBoard(int size){
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public boolean solve(){
+        return true;
+    }
+
+    public int countSolutions() {
+        return 0;
+    }
+
+}
