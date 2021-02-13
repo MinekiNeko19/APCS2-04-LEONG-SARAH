f19f9f4e1e96e8f6536509ebbd72eac601e59f0c
Sarah Leong <sleong20@stuy.edu>
2021-02-13 14:24:37 -0500

    addQueen: can add 1 queen

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -1,8 +1,18 @@
+import java.lang.IllegalStateException;
+
 public class QueenBoard {
     private int[][] board;
 
-    public boolean addQueen(int r, int c) {
-        // temporary
+    public boolean addQueen(int r, int c) { // temporarily public
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == -1) {
+                    // call removeQueen
+                    return false;
+                }
+                if (i == r || j == c) {board[i][j]+=1;}
+            }
+        }
         board[r][c] = -1;
         return true;
     }
@@ -19,7 +29,9 @@ public class QueenBoard {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (board[i][j]==-1) {ans += "Q";}
-                else {ans += "_";}
+                //temporary
+                else {ans += board[i][j];}
+                // else {ans += "_";}
                 if (j==board.length-1) {ans += "\n";}
                 else {ans += " ";}
             }
