9f293b514ce12cc91eab8d8710626f42a42d7237
Sarah Leong <sleong20@stuy.edu>
2021-02-13 14:40:06 -0500

    privated addQueen and removeQueen

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -3,7 +3,7 @@ import java.lang.IllegalStateException;
 public class QueenBoard {
     private int[][] board;
 
-    public boolean addQueen(int r, int c) { // temporarily public
+    private boolean addQueen(int r, int c) {
         if (board[r][c] != 0) return false; // if the spot is dangerous don't add
 
         for (int i = 0; i < board.length; i++) {
@@ -14,7 +14,7 @@ public class QueenBoard {
         board[r][c] = -1;
         return true;
     }
-    public void removeQueen(int r, int c) { // temporarily public
+    private void removeQueen(int r, int c) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (i == r || j == c) {board[i][j]-=1;} // marks which boxes are dangerous
