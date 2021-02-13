0049eb3dfe5d577adcc25c182c41af9a9aaff815
Sarah Leong <sleong20@stuy.edu>
2021-02-13 14:30:03 -0500

    addQueen: changed return false condition

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -4,13 +4,11 @@ public class QueenBoard {
     private int[][] board;
 
     public boolean addQueen(int r, int c) { // temporarily public
+        if (board[r][c] != 0) return false; // if the spot is dangerous don't add
+        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == -1) {
-                    // call removeQueen
-                    return false;
-                }
-                if (i == r || j == c) {board[i][j]+=1;}
+                if (i == r || j == c) {board[i][j]+=1;} // marks which boxes are dangerous
             }
         }
         board[r][c] = -1;
