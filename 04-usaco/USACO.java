d9856e3b49b21bb62fad77083d833dfd78bfa52a
Sarah Leong <sleong20@stuy.edu>
2021-03-06 12:47:14 -0500

    converted all input into the appropriate variables

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -1,8 +1,75 @@
-public class USACO {
-    public int bronze(String filename) {
+import java.util.*;
+import java.io.*;
 
+public class USACO {
+    public static int bronze(String filename) {
+        try {
+            // parse the file
+            File f = new File(filename);
+            Scanner in = new Scanner(f);
+            int rows = in.nextInt();
+            int cols = in.nextInt();
+            int e = in.nextInt();
+            int n = in.nextInt();
+            int[][] elevation = new int[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    elevation[i][j] = in.nextInt();
+                }
+            }
+            toString(elevation);
+            int[][] instructions = new int[n][3];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < 3; j++) {
+                    instructions[i][j] = in.nextInt();
+                }
+                // to adjust the coordinates to match 2d array dimensions
+                instructions[i][0] = instructions[i][0]-1;
+                instructions[i][1] = instructions[i][1]-1;
+            }
+            toString(instructions);
+
+            // run the instructions
+            int highest = 0;
+            for (int i = 0; i < instructions.length; i++) { // what instruction we are on
+                // find the highest elevation
+                for (int y = instructions[i][0]; y <= instructions[i][0]+2; y++) { //row
+                    for (int x = instructions[i][1]; x <= instructions[i][1]+2; x++) { // col
+                        if (elevation[y][x] > highest) {
+                            highest = elevation[y][x];
+                        }
+                    }
+                }
+                System.out.println(highest);
+                // change all the values to be level or lower
+                // highest = highest - instructions[i][2];
+                // if (highest < 0) highest = 0;
+                // System.out.println(highest);
+                // for (int y = instructions[i][0]; y <= instructions[i][0]+2; y++) { //row
+                //     for (int x = instructions[i][1]; x <= instructions[i][1]+2; x++) { // col
+                //         if (elevation[y][x] < highest) {
+                //             elevation[y][x] = highest;
+                //         }
+                //     }
+                // }
+                // toString(elevation);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
+        return 0;
     }
-    public long silver(String filename) {
-        
+    public static long silver(String filename) {
+        return 0;
+    }
+
+    private static void toString(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
