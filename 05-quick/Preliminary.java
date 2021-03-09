d8e9349884f39bc07c50b5f7bb414c6410b2e9c8
Sarah Leong <sleong20@stuy.edu>
2021-03-08 20:25:28 -0500

    pivot index and other data obtained successfully

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -7,6 +7,26 @@ public class Preliminary {
        *4. Only the indices from start to end inclusive are considered in range
        *@return the index of the final position of the pivot element.
        */
-      public static int partition (int [] data, int start, int end){
+    public static int partition (int [] data, int start, int end){
+        int index = (int)(Math.random() * (end-start+1))+start;
+        int pivot = data[index];
+        int current = data[0]; // to put the pivot in the first index
+        int last = data[end]; // check what happens if the index chooses the last
+        int temp = data[1];
+
+        System.out.println(
+            "index: " + index +
+            " pivot: " + pivot +
+            " current: " + current +
+            " last: " + last +
+            " temp: " + temp
+        );
+
+        // data[0] = pivot;
+        // data[index] = current;
+
+
+
+        return 0; 
     }
 }
