1500158b95df9093d427e6cba736a64278c4cffc
Sarah Leong <sleong20@stuy.edu>
2021-03-16 17:35:01 -0400

    only merges length two arrays

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -8,6 +8,20 @@ public class Merge {
     }
 
     private static void mergesort(int[] data, int[] temp, int lo, int hi) {
-        
+        if (hi-lo + 1 > 1) {
+            mergesort(data, temp, 0, data.length/2-1);
+            mergesort(data, temp, data.length/2, data.length-1);
+            System.out.println("running");
+            for (int i = lo;  i < hi-lo; i++) {
+                if (data[lo] > data[hi]) {
+                    System.out.println("lo val: " + data[lo] + " hi val: " + data[hi]);
+                    temp[hi] = data[lo];
+                    temp[lo] = data[hi];
+                }
+            }
+            for (int i = 0; i < data.length; i++) {
+                data[i] = temp[i];
+            }
+        }
     }
 }
