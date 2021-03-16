bbb3804ffca427a624051d6832c89a48a3c852c9
Sarah Leong <sleong20@stuy.edu>
2021-03-16 17:25:37 -0400

    initialized recursive call and copied over info to another int[]

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,6 +1,10 @@
 public class Merge {
     public static void mergesort(int[] data) {
-
+        int[] temp = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        mergesort(data, temp, 0, data.length-1);
     }
 
     private static void mergesort(int[] data, int[] temp, int lo, int hi) {
