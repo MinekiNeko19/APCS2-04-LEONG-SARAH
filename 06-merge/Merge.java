dd339c77f34f9c0f6849d0a4602c1c9ca8af2126
Sarah Leong <sleong20@stuy.edu>
2021-03-16 21:59:15 -0400

    cleaned up the code; merges only 2 element arrays

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -8,20 +8,30 @@ public class Merge {
     }
 
     private static void mergesort(int[] data, int[] temp, int lo, int hi) {
-        if (hi-lo + 1 > 1) {
-            mergesort(data, temp, 0, data.length/2-1);
-            mergesort(data, temp, data.length/2, data.length-1);
-            System.out.println("running");
-            for (int i = lo;  i < hi-lo; i++) {
-                if (data[lo] > data[hi]) {
-                    System.out.println("lo val: " + data[lo] + " hi val: " + data[hi]);
-                    temp[hi] = data[lo];
-                    temp[lo] = data[hi];
-                }
+        if (hi-lo >= 1) {
+            int leftstart = lo;
+            int rightstart = (hi-lo)/2;
+            System.out.println(rightstart + " left: " + leftstart);
+            // mergesort(data, temp, lo, leftend);
+            // mergesort(data, temp, rightstart, lo-1);
+
+            // merge code
+            if (hi-lo==1) {
+                temp[lo] = Math.min(data[lo], data[hi]);
+                temp[hi] = Math.max(data[lo], data[hi]);
+                System.out.println(temp[lo] + "split" + temp[hi]);
             }
+            
+
+            // transfer to original
             for (int i = 0; i < data.length; i++) {
                 data[i] = temp[i];
             }
+        }
+    }
+    private static void toString(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
         }
     }
 }
