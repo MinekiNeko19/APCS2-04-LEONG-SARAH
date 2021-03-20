027bdaeb57ea93d6b341fed17d0cf7769cc774d1
Sarah Leong <sleong20@stuy.edu>
2021-03-19 23:18:34 -0400

    fixed toString and added addFirst (wraparound not tested yet)

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,4 +1,4 @@
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class MyDeque<E> {
     private E[] data;
@@ -9,8 +9,8 @@ public class MyDeque<E> {
         E[] d = (E[])new Object[10];
         data = d;
         size = 0;
-        start = 0;
-        end = 0;
+        start = 5;
+        end = 5;
     }
     public MyDeque(int initialCapacity){
         @SuppressWarnings("unchecked")
@@ -25,19 +25,70 @@ public class MyDeque<E> {
     }
     public String toString(){
         String s = "{";
-        for (int i = 0; i < size; i++) {
-            s += data[i];
-            if (i < size-1) {
-                s += ", ";
+
+        //to look into the whole array (testing)
+        // for (int i = 0; i < data.length; i ++) {
+        //     s+=data[i];
+        //     if (i < data.length-1) {
+        //         s+= ", ";
+        //     }
+        // }
+
+        if (start < end) {
+            for (int i = start; i <= end; i++) {
+                s += data[i];
+                if (i < end-1) {
+                    s += ", ";
+                }
             }
         }
+        else {
+            for (int i = start; i != end+1; i++) {
+                s += data[i];
+                if (i != end) {
+                    s += ", ";
+                }
+            }
+        }
+
         return s + "}";
     }
     public void addFirst(E element){ // throws NullPointerException if element is null
+        if (element==null) throw new NullPointerException();
 
+        // if data capacity reached
+        size++;
+        if (size==data.length) {
+            @SuppressWarnings("unchecked")
+            E[] temp = (E[]) new Object[data.length*2+1];
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+
+        // add the value to the start
+        start--;
+        if (start == -1) {
+            start = data.length-1;
+        }
+        data[start] = element;
+        // special cases in the beginning
+        if (size == 1) {
+            end = start;
+        }
+        else if (size == 2) {
+            end = start + 1;
+        }
     }
     public void addLast(E element){ // throws NullPointerException if element is null
+        if (element==null) throw new NullPointerException();
 
+        // if data capacity reached
+
+        // add the value to the end
+
+        
     }
     public E removeFirst(){ // throws NoSuchElementException when empty
         return null;
