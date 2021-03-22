d0a18aed4787e62c2cd9af7b7cb323a11accda4f
Sarah Leong <sleong20@stuy.edu>
2021-03-22 12:43:26 -0400

    converts the string into tokens

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class Calculator {
     public static double eval(String s){
+        StringTokenizer line = new StringTokenizer(s);
+        while (line.hasMoreTokens()) {
+            System.out.println(line.nextToken());
+        }
         return 0;
     }
 }
