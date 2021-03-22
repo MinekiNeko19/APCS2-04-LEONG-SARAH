7b26049c868d6161a660f38eb1506bbef62b6200
Sarah Leong <sleong20@stuy.edu>
2021-03-22 13:23:45 -0400

    added remainder and exceptions

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,35 +1,49 @@
 import java.util.*;
 
+
 public class Calculator {
     public static double eval(String s){
+        if (s.equals("")) throw new IllegalArgumentException(); // change if unneeded
+
         Scanner line = new Scanner(s);
         ArrayDeque<Double> stack = new ArrayDeque<Double>();
         while (line.hasNext()) {
             String current = line.next();
             // System.out.println(current);
             if (current.equals("*")) {
+                if (stack.size() < 2) throw new IllegalArgumentException();
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(a*b);
             }
             else if (current.equals("/")) {
+                if (stack.size() < 2) throw new IllegalArgumentException();
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(b/a);
             }
             else if (current.equals("+")) {
+                if (stack.size() < 2) throw new IllegalArgumentException();
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(a+b);
             }
             else if (current.equals("-")) {
+                if (stack.size() < 2) throw new IllegalArgumentException();
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(b-a);
-            } else {
+            }
+            else if (current.equals("%")) {
+                if (stack.size() < 2) throw new IllegalArgumentException();
+                Double a = stack.removeFirst();
+                Double b = stack.removeFirst();
+                stack.addFirst(b%a);
+            }
+            else {
                 stack.addFirst(Double.parseDouble(current));
             }
-            System.out.println(stack.toString());
+            // System.out.println(stack.toString());
         }
         return stack.getLast();
     }
