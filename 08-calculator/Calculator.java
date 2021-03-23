937315773b30688a8ad7c23397e7ba2033ba6b4c
Sarah Leong <sleong20@stuy.edu>
2021-03-23 15:20:44 -0400

    added text for each exception

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Calculator {
     public static double eval(String s){
-        if (s.equals("")) throw new IllegalArgumentException(); // change if unneeded
+        if (s.equals("")) throw new IllegalArgumentException("Nothing to calculate (empty)"); // change if unneeded
 
         Scanner line = new Scanner(s);
         ArrayDeque<Double> stack = new ArrayDeque<Double>();
@@ -11,31 +11,31 @@ public class Calculator {
             String current = line.next();
             // System.out.println(current);
             if (current.equals("*")) {
-                if (stack.size() < 2) throw new IllegalArgumentException();
+                if (stack.size() < 2) throw new IllegalArgumentException("Too many operands");
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(a*b);
             }
             else if (current.equals("/")) {
-                if (stack.size() < 2) throw new IllegalArgumentException();
+                if (stack.size() < 2) throw new IllegalArgumentException("Too many operands");
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(b/a);
             }
             else if (current.equals("+")) {
-                if (stack.size() < 2) throw new IllegalArgumentException();
+                if (stack.size() < 2) throw new IllegalArgumentException("Too many operands");
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(a+b);
             }
             else if (current.equals("-")) {
-                if (stack.size() < 2) throw new IllegalArgumentException();
+                if (stack.size() < 2) throw new IllegalArgumentException("Too many operands");
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(b-a);
             }
             else if (current.equals("%")) {
-                if (stack.size() < 2) throw new IllegalArgumentException();
+                if (stack.size() < 2) throw new IllegalArgumentException("Too many operands");
                 Double a = stack.removeFirst();
                 Double b = stack.removeFirst();
                 stack.addFirst(b%a);
@@ -46,7 +46,7 @@ public class Calculator {
             // System.out.println(stack.toString());
         }
         line.close();
-        if (stack.size() != 1) throw new IllegalArgumentException();
+        if (stack.size() != 1) throw new IllegalArgumentException("Too few operands");
         return stack.getLast();
     }
 }
