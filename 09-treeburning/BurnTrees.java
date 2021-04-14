8da83dfcba3aa6f4d61a6c8fa3871d6fb1474980
Sarah Leong <sleong20@stuy.edu>
2021-04-14 11:11:43 -0400

    fixed constructor for frontiers; issue with ticking

--- 09-treeburning/BurnTrees.java
+++ 09-treeburning/BurnTrees.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class BurnTrees{
-  private int[][]map;
+public class BurnTrees {
+  private int[][] map;
   private int ticks;
   // private int fireCount; // keeps track of the number of fires active for loop
   // private static int EMBER = 5; // notates will be set on fire next for loop
@@ -12,185 +12,210 @@ public class BurnTrees{
   private static int ASH = 3;
   private static int SPACE = 0;
 
-
-  /*DO NOT UPDATE THIS
-   *PLEASE READ SO YOU SEE HOW THE SIMULATION IS SUPPOSED TO WORK!!!
+  /*
+   * DO NOT UPDATE THIS PLEASE READ SO YOU SEE HOW THE SIMULATION IS SUPPOSED TO
+   * WORK!!!
    */
-  public int run(){
-    while(!done()){
+  public int run() {
+    while (!done()) {
       tick();
     }
     return getTicks();
   }
 
-  /*Initialize the simulation.
-   *If you add more instance variables you can add more here,
-   *otherwise it is complete
+  /*
+   * Initialize the simulation. If you add more instance variables you can add
+   * more here, otherwise it is complete
    */
-  public BurnTrees(int width,int height, double density){
+  public BurnTrees(int width, int height, double density) {
+    FrontierCol = new ArrayDeque<Integer>();
+    FrontierRow = new ArrayDeque<Integer>();
     map = new int[height][width];
-    for(int r=0; r<map.length; r++ )
-      for(int c=0; c<map[r].length; c++ )
-        if(Math.random() < density) {
-           map[r][c]=2;
-           FrontierRow.add(r);
-           FrontierCol.add(c);
-          //  if (c==0) fireCount++;
+    for (int r = 0; r < map.length; r++)
+      for (int c = 0; c < map[r].length; c++)
+        if (Math.random() < density) {
+          map[r][c] = 2;
+          FrontierRow.add(r);
+          FrontierCol.add(c);
+          // if (c==0) fireCount++;
 
         }
-    start();//set the left column on fire.
+    start();// set the left column on fire.
   }
 
-  /*Determine if the simulation is still burning
-   *@return true if any fires are still burning, false otherwise
+  /*
+   * Determine if the simulation is still burning
+   * 
+   * @return true if any fires are still burning, false otherwise
    */
-  public boolean done(){
+  public boolean done() {
     // return fireCount <= 0;
     return FrontierRow.size() <= 0;
   }
 
-
-  /*This is the core of the simulation. All of the logic for advancing to the next round goes here.
-   *All existing fires spread new fires, and turn to ash
-   *new fires should remain fire, and not spread.
+  /*
+   * This is the core of the simulation. All of the logic for advancing to the
+   * next round goes here. All existing fires spread new fires, and turn to ash
+   * new fires should remain fire, and not spread.
    */
-  public void tick(){
+  public void tick() {
     ticks++;
     // for loop version
     // for (int i = 0; i < map.length; i++) {
-    //   for (int j = 0; j < Math.min(map[i].length, (ticks+1)); j++) {
-    //       if (map[i][j]==FIRE) {
-    //         if (i > 0 && map[i-1][j]==TREE) { // checks up
-    //           map[i-1][j]=FIRE;
-    //           fireCount++;
-    //         }
-    //         if (i < map.length-1 && map[i+1][j]==TREE) { // checks down
-    //           map[i+1][j]=EMBER;
-    //           fireCount++;
-    //         }
-    //         if (j > 0 && map[i][j-1]==TREE) { // checks left
-    //           map[i][j-1]=FIRE;
-    //           fireCount++;
-    //         }
-    //         if (j < map[i].length-1 && map[i][j+1]==TREE) { // checks right
-    //           map[i][j+1]=EMBER;
-    //           fireCount++;
-    //         }
-    //         map[i][j] = ASH;
-    //         fireCount--;
-    //       }    
-    //       if (map[i][j]==EMBER) {
-    //         map[i][j] = FIRE;
-    //       }
-    //   }
+    // for (int j = 0; j < Math.min(map[i].length, (ticks+1)); j++) {
+    // if (map[i][j]==FIRE) {
+    // if (i > 0 && map[i-1][j]==TREE) { // checks up
+    // map[i-1][j]=FIRE;
+    // fireCount++;
     // }
-    
-    
-  }
-
-  /*
-   *Sets the trees in the left column of the forest on fire
-   */
-  public void start(){
-    //If you add more instance variables you can add more here,
-    //otherwise it is complete.
-    for(int i = 0; i < map.length; i++){
-      if(map[i][0]==TREE){
-        map[i][0]=FIRE;
+    // if (i < map.length-1 && map[i+1][j]==TREE) { // checks down
+    // map[i+1][j]=EMBER;
+    // fireCount++;
+    // }
+    // if (j > 0 && map[i][j-1]==TREE) { // checks left
+    // map[i][j-1]=FIRE;
+    // fireCount++;
+    // }
+    // if (j < map[i].length-1 && map[i][j+1]==TREE) { // checks right
+    // map[i][j+1]=EMBER;
+    // fireCount++;
+    // }
+    // map[i][j] = ASH;
+    // fireCount--;
+    // }
+    // if (map[i][j]==EMBER) {
+    // map[i][j] = FIRE;
+    // }
+    // }
+    // }
+    // using queues
+    int len = FrontierCol.size();
+    System.out.println(len);
+    for (int i = 0; i < len; i++) {
+      int r = FrontierRow.remove();
+      int c = FrontierCol.remove();
+      if (map[r][c] == FIRE) {
+        if (r > 0 && map[r - 1][c] == TREE) { // checks up
+          map[r - 1][c] = FIRE;
+          FrontierRow.add(r-1);
+          FrontierCol.add(c);
+        }
+        if (r < map.length - 1 && map[r + 1][c] == TREE) { // checks down
+          map[r + 1][c] = FIRE;
+          FrontierRow.add(r+1);
+          FrontierCol.add(c);
+        }
+        if (c > 0 && map[r][c - 1] == TREE) { // checks left
+          map[r][c - 1] = FIRE;
+          FrontierRow.add(r);
+          FrontierCol.add(c-1);
+        }
+        if (c < map[r].length - 1 && map[r][c + 1] == TREE) { // checks right
+          map[r][c + 1] = FIRE;
+          FrontierRow.add(r);
+          FrontierCol.add(c+1);
+        }
+        map[r][c] = ASH;
       }
     }
   }
 
+  /*
+   * Sets the trees in the left column of the forest on fire
+   */
+  public void start() {
+    // If you add more instance variables you can add more here,
+    // otherwise it is complete.
+    for (int i = 0; i < map.length; i++) {
+      if (map[i][0] == TREE) {
+        map[i][0] = FIRE;
+      }
+    }
+  }
 
-
-
-
-  /*DO NOT UPDATE THIS*/
-  public int getTicks(){
+  /* DO NOT UPDATE THIS */
+  public int getTicks() {
     return ticks;
   }
 
-  /*DO NOT UPDATE THIS*/
-  public String toString(){
+  /* DO NOT UPDATE THIS */
+  public String toString() {
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < map.length; i++) {
       for (int c = 0; c < map[i].length; c++) {
-        if(map[i][c]==SPACE)
+        if (map[i][c] == SPACE)
           builder.append(" ");
-        else if(map[i][c]==TREE)
+        else if (map[i][c] == TREE)
           builder.append("@");
-        else if(map[i][c]==FIRE)
+        else if (map[i][c] == FIRE)
           builder.append("w");
-        else if(map[i][c]==ASH)
+        else if (map[i][c] == ASH)
           builder.append(".");
       }
       builder.append("\n");
     }
     return builder.toString();
   }
-  /*DO NOT UPDATE THIS*/
-  public String toStringColor(){
+
+  /* DO NOT UPDATE THIS */
+  public String toStringColor() {
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < map.length; i++) {
       for (int c = 0; c < map[i].length; c++) {
-        if(map[i][c]==0)
+        if (map[i][c] == 0)
           builder.append(" ");
-        else if(map[i][c]==2)
-          builder.append(Text.color(Text.GREEN)+"@");
-        else if(map[i][c]==1)
-          builder.append(Text.color(Text.RED)+"w");
-        else if(map[i][c]==3)
-          builder.append(Text.color(Text.DARK)+".");
+        else if (map[i][c] == 2)
+          builder.append(Text.color(Text.GREEN) + "@");
+        else if (map[i][c] == 1)
+          builder.append(Text.color(Text.RED) + "w");
+        else if (map[i][c] == 3)
+          builder.append(Text.color(Text.DARK) + ".");
       }
-      builder.append("\n"+Text.RESET);
+      builder.append("\n" + Text.RESET);
     }
-    return builder.toString()+ticks+"\n";
+    return builder.toString() + ticks + "\n";
   }
 
-
-  /*DO NOT UPDATE THIS*/
+  /* DO NOT UPDATE THIS */
   public int animate(int delay) {
     System.out.print(Text.CLEAR_SCREEN);
-    System.out.println(Text.go(1,1)+toStringColor());
+    System.out.println(Text.go(1, 1) + toStringColor());
     Text.wait(delay);
-    while(!done()){
+    while (!done()) {
       tick();
-      System.out.println(Text.go(1,1)+toStringColor());
+      System.out.println(Text.go(1, 1) + toStringColor());
       Text.wait(delay);
     }
     return getTicks();
   }
 
-  /*DO NOT UPDATE THIS*/
-  public int outputAll(){
+  /* DO NOT UPDATE THIS */
+  public int outputAll() {
     System.out.println(toString());
-    while(!done()){
+    while (!done()) {
       tick();
       System.out.println(toString());
     }
     return getTicks();
   }
 
-
-  public static void main(String[]args)  throws InterruptedException{
+  public static void main(String[] args) throws InterruptedException {
     int WIDTH = 20;
     int HEIGHT = 20;
     int DELAY = 200;
     double DENSITY = .7;
-    if(args.length > 1){
+    if (args.length > 1) {
       WIDTH = Integer.parseInt(args[0]);
       HEIGHT = Integer.parseInt(args[1]);
       DENSITY = Double.parseDouble(args[2]);
     }
-    if(args.length > 3){
+    if (args.length > 3) {
       DELAY = Integer.parseInt(args[3]);
     }
-    BurnTrees b = new BurnTrees(WIDTH,HEIGHT,DENSITY);
+    BurnTrees b = new BurnTrees(WIDTH, HEIGHT, DENSITY);
 
-
-    System.out.println(b.animate(DELAY));//animate all screens and print the final answer
+    System.out.println(b.animate(DELAY));// animate all screens and print the final answer
     // System.out.println(b.outputAll());//print all screens and the final answer
   }
-
 
 }
