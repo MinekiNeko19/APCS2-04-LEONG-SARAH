9a054faa70b60544ce413ff949cfedf02d5b5f47
Sarah Leong <sleong20@stuy.edu>
2021-04-21 23:45:28 -0400

    fixed pushdown to actually only pushdown

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,24 +1,26 @@
 public class MyHeap {
     // index of a node: i*2 + 1or2
     private static void pushDown(int[] data, int size, int index) {
-        if (index <= size/2) {
-            int leftInd = index*2 + 1;
-            int rightInd = index*2 + 2;
-            // System.out.println("Left " + data[leftInd]);
-            // System.out.println("right " + data[rightInd]);
-            // System.out.println("pivot " + data[index]);
-            if (data[leftInd] > data[index] && data[leftInd] > data[rightInd]) {
-                int temp = data[leftInd];
-                data[leftInd] = data[index];
-                data[index] = temp;
-            }
-            else if (data[rightInd] > data[index]) {
+        if (size > 1) {
+            int leftsize = size/2;
+            int rightsize = size/2;
+            if (size % 2 == 1) leftsize=leftsize+1;
+
+            int leftInd = index*2;
+            int rightInd = index*2 + 1;
+
+            if (rightInd < data.length && data[rightInd] > data[index]) {
                 int temp = data[rightInd];
                 data[rightInd] = data[index];
                 data[index] = temp;
+                pushDown(data, rightsize, rightInd);
             }
-            pushDown(data, size/2, leftInd);
-            pushDown(data, size/2, rightInd);
+            if (data[leftInd] > data[index]) {
+                int temp = data[leftInd];
+                data[leftInd] = data[index];
+                data[index] = temp;
+                pushDown(data, leftsize, leftInd);
+            }
         }
     }  
     private static void buildHeap(int[] data) {
@@ -43,14 +45,15 @@ public class MyHeap {
     public static void main(String[] args) {
         // comment out later this is for private testing
         int[] a = new int[]{1,23,4,7,10}; // buildHeap order: 23, 10, 4, 7, 1
-        int[] b = new int[]{2,19,100,3,25,1,17,19,36};
+        // int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
+        int[] c = new int[]{1,40,22,10,3};
 
-        toString(a); //toString works as intended
-        buildHeap(a);
-        toString(a);
+        // toString(a); //toString works as intended
+        // buildHeap(a);
+        // toString(a);
 
-        toString(b);
-        buildHeap(b);
-        toString(b);
+        toString(c);
+        buildHeap(c);
+        toString(c);
     }
 }
