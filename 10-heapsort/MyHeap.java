0859fcfbebde7909ad9c63ceb388b59b35764ea3
Sarah Leong <sleong20@stuy.edu>
2021-04-21 23:59:08 -0400

    swaps with the greater of children

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -6,19 +6,24 @@ public class MyHeap {
             int rightsize = size/2;
             if (size % 2 == 1) leftsize=leftsize+1;
 
-            int leftInd = index*2;
-            int rightInd = index*2 + 1;
+            int leftInd = index*2+1;
+            int rightInd = index*2 + 2;
+            // System.out.println("right " + data[rightInd]);
+            // System.out.println("left " + data[leftInd]);
+        
 
-            if (rightInd < data.length && data[rightInd] > data[index]) {
+            if (rightInd < data.length && data[rightInd] > data[index] && data[rightInd] > data[leftInd]) {
                 int temp = data[rightInd];
                 data[rightInd] = data[index];
                 data[index] = temp;
+                toString(data);
                 pushDown(data, rightsize, rightInd);
             }
-            if (data[leftInd] > data[index]) {
+            if (leftInd < data.length && data[leftInd] > data[index] && data[leftInd] > data[rightInd]) {
                 int temp = data[leftInd];
                 data[leftInd] = data[index];
                 data[index] = temp;
+                toString(data);
                 pushDown(data, leftsize, leftInd);
             }
         }
@@ -47,13 +52,14 @@ public class MyHeap {
         int[] a = new int[]{1,23,4,7,10}; // buildHeap order: 23, 10, 4, 7, 1
         // int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
         int[] c = new int[]{1,40,22,10,3};
+        int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6};
 
         // toString(a); //toString works as intended
         // buildHeap(a);
         // toString(a);
 
-        toString(c);
-        buildHeap(c);
-        toString(c);
+        toString(d);
+        buildHeap(d);
+        toString(d);
     }
 }
