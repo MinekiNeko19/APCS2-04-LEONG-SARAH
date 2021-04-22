3a1331159395894f83fe167ba65f9c7ab1a1625a
Sarah Leong <sleong20@stuy.edu>
2021-04-22 16:01:09 -0400

    removed the need for the nesting in buildHeap

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -17,23 +17,25 @@ public class MyHeap {
                 data[rightInd] = data[index];
                 data[index] = temp;
                 // toString(data);
-                pushDown(data, rightsize, rightInd);
+                // pushDown(data, rightsize, rightInd);
             }
             if (leftInd < data.length && data[leftInd] > data[index] && data[leftInd] > data[rightInd]) {
                 int temp = data[leftInd];
                 data[leftInd] = data[index];
                 data[index] = temp;
                 // toString(data);
-                pushDown(data, leftsize, leftInd);
+                // pushDown(data, leftsize, leftInd);
             }
+            pushDown(data, leftsize, leftInd);            
+            pushDown(data, rightsize, rightInd);
         }
     }  
     private static void buildHeap(int[] data) {
-        for (int j = 0; j < 2; j++) {
+        // for (int j = 0; j < 2; j++) {
             for (int i = 0; i < data.length; i++) {
                 pushDown(data, data.length, i);
             }
-        }
+        // }
     }
 
     private static void remove(int[] data, int index) {
@@ -43,28 +45,45 @@ public class MyHeap {
 
     }
     private static void toString(int[]data) {
-        String arr = "[" + data[0];
-        for (int i = 1; i < data.length; i++) {
-            arr += ", " + data[i];
-        }
-        arr += "]";
-        System.out.println(arr);
-    }
+    //     String arr = "[" + data[0];
+    //     for (int i = 1; i < data.length; i++) {
+    //         arr += ", " + data[i];
+    //     }
+    //     arr += "]";
+    //     System.out.println(arr);
+    // }
 
-    public static void main(String[] args) {
-        // comment out later this is for private testing
-        int[] a = new int[]{1,23,4,7,10}; // buildHeap order: 23, 10, 4, 7, 1
-        int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
-        int[] c = new int[]{1,40,22,10,3};
-        int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6};
-        int[] e = new int[]{10,20,50,15,9,40,30,2,3,5,6};
+    // public static void main(String[] args) {
+    //     // comment out later this is for private testing
+    //     int[] a = new int[]{1,23,4,7,10}; // 23, 10, 4, 7, 1
+    //     int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
+    //     int[] c = new int[]{1,40,22,10,3}; // 40, 10, 22, 1, 3
+    //     int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,1,30,2,3,5,6
+    //     int[] e = new int[]{10,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,10,30,2,3,5,6
 
-        // toString(a); //toString works as intended
-        // buildHeap(a);
-        // toString(a);
+    //     System.out.println("a");
+    //     toString(a);
+    //     buildHeap(a);
+    //     toString(a);
 
-        toString(b);
-        buildHeap(b);
-        toString(b);
+    //     System.out.println("b");
+    //     toString(b);
+    //     buildHeap(b);
+    //     toString(b);
+        
+    //     System.out.println("c");
+    //     toString(c);
+    //     buildHeap(c);
+    //     toString(c);
+        
+    //     System.out.println("d");
+    //     toString(d);
+    //     buildHeap(d);
+    //     toString(d);
+        
+    //     System.out.println("e");
+    //     toString(e);
+    //     buildHeap(e);
+    //     toString(e);
     }
 }
