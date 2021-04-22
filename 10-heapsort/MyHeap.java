942915c47cf137dc9e9948a792fae7a7d609da96
Sarah Leong <sleong20@stuy.edu>
2021-04-21 20:45:50 -0400

    method frameworks

---
+++ 10-heapsort/MyHeap.java
@@ -0,0 +1,19 @@
+public class MyHeap {
+    private static void pushDown(int[] data, int size, int index) {
+
+    }  
+    private static void buildHeap(int[] data) {
+
+    }
+
+    private static void remove(int[] data, int index) {
+
+    }
+    public static void heapsort(int[] data) {
+
+    }
+
+    public static void main(String[] args) { // comment out later this is for private testing
+
+    }
+}
