c7db78b534d4703fc3e7869fffdbdc0b2297552e
Sarah Leong <sleong20@stuy.edu>
2021-04-21 21:38:57 -0400

    only top level was properly switched

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,9 +1,25 @@
 public class MyHeap {
+    // index of a node: i*2 + 1or2
     private static void pushDown(int[] data, int size, int index) {
-
+        if (index <= size/2) {
+            int leftInd = index*2 + 1;
+            int rightInd = index*2 + 2;
+            pushDown(data, size/2, leftInd);
+            pushDown(data, size/2, rightInd);
+            if (data[leftInd] > data[index]) {
+                int temp = data[leftInd];
+                data[leftInd] = data[index];
+                data[index] = temp;
+            }
+            if (data[rightInd] > data[index]) {
+                int temp = data[rightInd];
+                data[rightInd] = data[index];
+                data[index] = temp;
+            }
+        }
     }  
     private static void buildHeap(int[] data) {
-
+        pushDown(data, data.length, 0);
     }
 
     private static void remove(int[] data, int index) {
@@ -12,8 +28,22 @@ public class MyHeap {
     public static void heapsort(int[] data) {
 
     }
+    private static void toString(int[]data) {
+        String arr = "[" + data[0];
+        for (int i = 1; i < data.length; i++) {
+            arr += ", " + data[i];
+        }
+        arr += "]";
+        System.out.println(arr);
+    }
 
-    public static void main(String[] args) { // comment out later this is for private testing
+    public static void main(String[] args) {
+        // comment out later this is for private testing
+        int[] a = new int[]{1,23,4,7,10}; // buildHeap order: 23, 10, 4, 7, 1
+        int[] b = new int[]{1,1,1,1,1,1};
 
+        toString(a); //toString works as intended
+        buildHeap(a);
+        toString(a);
     }
 }
