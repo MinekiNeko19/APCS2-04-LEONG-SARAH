b272b937ccb7f62caf8e2c4c6ec86d48b82a361d
Sarah Leong <sleong20@stuy.edu>
2021-04-24 00:15:42 -0400

    improved the logic in pushDown

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,48 +1,86 @@
 public class MyHeap {
     // index of a node: i*2 + 1or2
     private static void pushDown(int[] data, int size, int index) {
-        if (size > 1) {
-            int leftsize = size/2;
-            int rightsize = size/2;
-            if (size % 2 == 1) leftsize=leftsize+1;
+        int leftInd = index*2 + 1;
+        int rightInd = index*2 + 2;
 
-            int leftInd = index*2+1;
-            int rightInd = index*2 + 2;
-            // System.out.println("right " + data[rightInd]);
-            // System.out.println("left " + data[leftInd]);
-        
+        if (leftInd < size) {
+            if (rightInd < size) {
+                if (data[rightInd] > data[index] && data[rightInd] > data[leftInd]) {
+                    int temp = data[rightInd];
+                    data[rightInd] = data[index];
+                    data[index] = temp;
 
-            if (rightInd < data.length && data[rightInd] > data[index] && data[rightInd] > data[leftInd]) {
-                int temp = data[rightInd];
-                data[rightInd] = data[index];
-                data[index] = temp;
-                // toString(data);
-                pushDown(data, rightsize, rightInd);
-                pushDown(data, leftsize, leftInd); 
+                    pushDown(data, size/2, rightInd);
+                }
+                if (data[leftInd] > data[index] && data[rightInd] < data[leftInd]) {
+                    int temp = data[leftInd];
+                    data[leftInd] = data[index];
+                    data[index] = temp;
+
+                    pushDown(data, size/2, leftInd);
+                }
             }
-            if (leftInd < data.length && data[leftInd] > data[index] && data[leftInd] > data[rightInd]) {
+            if (data[leftInd] > data[index]) {
                 int temp = data[leftInd];
                 data[leftInd] = data[index];
                 data[index] = temp;
-                // toString(data);
-                pushDown(data, leftsize, leftInd);
-                pushDown(data, rightsize, rightInd);
             }
-            // pushDown(data, leftsize, leftInd);
-            // pushDown(data, rightsize, rightInd);
         }
+
+        // if (size > 1) {
+        //     int leftsize = size/2;
+        //     int rightsize = size/2;
+        //     if (size % 2 == 1) leftsize=leftsize+1;
+
+        //     int leftInd = index*2+1;
+        //     int rightInd = index*2 + 2;
+        //     // System.out.println("right " + data[rightInd]);
+        //     // System.out.println("left " + data[leftInd]);
+        
+
+        //     if (rightInd < data.length && data[rightInd] > data[index] && data[rightInd] > data[leftInd]) {
+        //         int temp = data[rightInd];
+        //         data[rightInd] = data[index];
+        //         data[index] = temp;
+        //         // toString(data);
+        //         pushDown(data, rightsize, rightInd);
+        //         // pushDown(data, leftsize, leftInd);
+        //     }
+        //     if (leftInd < data.length && data[leftInd] > data[index] && data[leftInd] > data[rightInd]) {
+        //         int temp = data[leftInd];
+        //         data[leftInd] = data[index];
+        //         data[index] = temp;
+        //         // toString(data);
+        //         pushDown(data, leftsize, leftInd);
+        //         // pushDown(data, rightsize, rightInd);
+        //     }
+        //     // pushDown(data, leftsize, leftInd);
+        //     // pushDown(data, rightsize, rightInd);
+        // }
     }  
     private static void buildHeap(int[] data) {
-        for (int i = 0; i < data.length; i++) {
+        for (int i = data.length-1; i >= 0; i--) {
             pushDown(data, data.length, i);
         }
     }
 
     private static void remove(int[] data, int index) {
+        int temp = data[index];
+        data[index] = data[0];
+        data[0] = temp;
 
+        // System.out.println("temp " + temp);
+        // System.out.println("index " + data[index]);
+        // System.out.println("0 " + data[0]);
+        pushDown(data, index-1, 0);
     }
-    public static void heapsort(int[] data) {
 
+    public static void heapsort(int[] data) {
+        buildHeap(data);
+        for (int i = data.length-1; i >= 0; i--) {
+            remove(data, i);
+        }
     }
     private static void toString(int[]data) {
         String arr = "[" + data[0];
@@ -55,35 +93,41 @@ public class MyHeap {
 
     public static void main(String[] args) {
         // comment out later this is for private testing
-        // int[] a = new int[]{1,23,4,7,10}; // 23, 10, 4, 7, 1
-        // int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
-        // int[] c = new int[]{1,40,22,10,3}; // 40, 10, 22, 1, 3
-        // int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,1,30,2,3,5,6
-        // int[] e = new int[]{10,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,10,30,2,3,5,6
+        int[] a = new int[]{1,23,4,7,10}; // 23, 10, 4, 7, 1
+        int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
+        int[] c = new int[]{1,40,22,10,3}; // 40, 10, 22, 1, 3
+        int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,1,30,2,3,5,6
+        int[] e = new int[]{10,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,10,30,2,3,5,6
+        int[] f = new int[]{20,15,8,10,5,7,6,2,9,1};
 
-        // System.out.println("a");
-        // toString(a);
-        // buildHeap(a);
-        // toString(a);
+        System.out.println("a");
+        toString(a);
+        buildHeap(a);
+        toString(a);
 
-        // System.out.println("b");
-        // toString(b);
-        // buildHeap(b);
-        // toString(b);
+        System.out.println("b");
+        toString(b);
+        buildHeap(b);
+        toString(b);
         
-        // System.out.println("c");
-        // toString(c);
-        // buildHeap(c);
-        // toString(c);
+        System.out.println("c");
+        toString(c);
+        buildHeap(c);
+        toString(c);
         
-        // System.out.println("d");
-        // toString(d);
-        // buildHeap(d);
-        // toString(d);
+        System.out.println("d");
+        toString(d);
+        buildHeap(d);
+        toString(d);
         
-        // System.out.println("e");
-        // toString(e);
-        // buildHeap(e);
-        // toString(e);
+        System.out.println("e");
+        toString(e);
+        buildHeap(e);
+        toString(e);
+
+        System.out.println("a");
+        toString(a);
+        heapsort(a);
+        toString(a);
     }
 }
