850704db7c2c567cd6c3f2bede1f8f2b24e4506c
Sarah Leong <sleong20@stuy.edu>
2021-04-24 00:38:24 -0400

    commented out testing

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -58,41 +58,41 @@ public class MyHeap {
 
     public static void main(String[] args) {
         // comment out later this is for private testing
-        int[] a = new int[]{1,23,4,7,10}; // 23, 10, 4, 7, 1
-        int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
-        int[] c = new int[]{1,40,22,10,3}; // 40, 10, 22, 1, 3
-        int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,1,30,2,3,5,6
-        int[] e = new int[]{10,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,10,30,2,3,5,6
-        int[] f = new int[]{20,15,8,10,5,7,6,2,9,1};
+        // int[] a = new int[]{1,23,4,7,10}; // 23, 10, 4, 7, 1
+        // int[] b = new int[]{2,19,100,3,25,1,17,7,36}; // 100, 19, 36, 17, 3, 25, 1, 2, 7
+        // int[] c = new int[]{1,40,22,10,3}; // 40, 10, 22, 1, 3
+        // int[] d = new int[]{1,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,1,30,2,3,5,6
+        // int[] e = new int[]{10,20,50,15,9,40,30,2,3,5,6}; // 50,20,40,15,9,10,30,2,3,5,6
+        // int[] f = new int[]{20,15,8,10,5,7,6,2,9,1};
 
-        System.out.println("a");
-        toString(a);
-        buildHeap(a);
-        toString(a);
+        // System.out.println("a");
+        // toString(a);
+        // buildHeap(a);
+        // toString(a);
 
-        System.out.println("b");
-        toString(b);
-        buildHeap(b);
-        toString(b);
+        // System.out.println("b");
+        // toString(b);
+        // buildHeap(b);
+        // toString(b);
         
-        System.out.println("c");
-        toString(c);
-        buildHeap(c);
-        toString(c);
+        // System.out.println("c");
+        // toString(c);
+        // buildHeap(c);
+        // toString(c);
         
-        System.out.println("d");
-        toString(d);
-        buildHeap(d);
-        toString(d);
+        // System.out.println("d");
+        // toString(d);
+        // buildHeap(d);
+        // toString(d);
         
-        System.out.println("e");
-        toString(e);
-        buildHeap(e);
-        toString(e);
+        // System.out.println("e");
+        // toString(e);
+        // buildHeap(e);
+        // toString(e);
 
-        System.out.println("a");
-        toString(a);
-        heapsort(a);
-        toString(a);
+        // System.out.println("f");
+        // toString(f);
+        // heapsort(f);
+        // toString(f);
     }
 }
